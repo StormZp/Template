8ac2dfe1ccc355eea5107f7aa833eff14d3fdf26
StormZp <409348950@qq.com>
2020-01-20 11:36:59 +0800

    basefragment

--- app/src/main/java/com/storm/template/base/BaseFragment.java
+++ app/src/main/java/com/storm/template/base/BaseFragment.java
@@ -2,8 +2,13 @@ package com.storm.template.base;
 
 import androidx.databinding.ViewDataBinding;
 
-import com.storm.mylibrary.base.SuperBaseViewModel;
 import com.storm.mylibrary.base.SuperBaseFragment;
+import com.storm.template.app.AppViewModelFactory;
 
-public abstract class BaseFragment<V extends ViewDataBinding, VM extends SuperBaseViewModel> extends SuperBaseFragment<V,VM> {
+public abstract class BaseFragment<V extends ViewDataBinding, VM extends BaseViewModel> extends SuperBaseFragment<V,VM> {
+    @Override
+    protected void initData() {
+        super.initData();
+        mViewModel.setRepository(AppViewModelFactory.getInstance().getRepository());
+    }
 }
